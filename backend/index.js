ecdc4a55efa4cf4baa8c776b405264918c4e31dc
Mostafa Rafiki <rafikimostafa@gmail.com>
2024-10-15 19:55:46 +0100

    Implement Product APIs for enhanced product management functionality - Developed productController.js to handle CRUD operations for products. - Added productModel.js to define the product schema for MongoDB. - Created productRoutes.js for routing product-related requests. - Updated index.js to include product routes in the main application. - Introduced checkId.js middleware for validating product ID formats. - Enhanced categoryController.js to ensure compatibility with product   management.

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,14 @@
 //packages
-import path from 'path';
-import express from 'express';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 //utiles
-import connectDB from './config/db.js';
-import userRoutes from './routes/userRoutes.js'
-import categoryRoutes from './routes/categoryRoutes.js'
+import connectDB from "./config/db.js";
+import userRoutes from "./routes/userRoutes.js";
+import categoryRoutes from "./routes/categoryRoutes.js";
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -21,6 +22,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
-app.use('/api/category', categoryRoutes);
+app.use("/api/category", categoryRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
