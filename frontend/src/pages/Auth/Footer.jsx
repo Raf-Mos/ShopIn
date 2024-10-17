bdd834394ee3d70175d52bef3accdaa7e3cf7ca5
Mostafa Rafiki <rafikimostafa@gmail.com>
2024-10-17 23:14:51 +0100

    Fix footer position to ensure consistent layout across all pages
    - Adjusted styles in `Footer.jsx` to resolve alignment issues, ensuring
    the footer appears correctly at the bottom of each page.
    - Modified `index.css` to include responsive design elements,
    	improving usability on different screen sizes.
    	- Updated `App.jsx` to ensure the footer is consistently rendered
    	across all routes.
    
    	These changes enhance the overall user experience by ensuring that
    	the footer is always positioned correctly, providing easier access
    	to important links and information.

--- frontend/src/pages/Auth/Footer.jsx
+++ frontend/src/pages/Auth/Footer.jsx
@@ -1,4 +1,3 @@
-
 const Footer = () => {
   return (
     <div>
@@ -13,9 +12,21 @@ const Footer = () => {
             <div>
               <h3 className="text-xl font-semibold mb-4">Quick Links</h3>
               <ul className="space-y-2">
-                <li><a href="/" className="hover:text-indigo-400">Home</a></li>
-                <li><a href="/shop" className="hover:text-indigo-400">Products</a></li>
-                <li><a href="/about-us" className="hover:text-indigo-400">About Us</a></li>
+                <li>
+                  <a href="/" className="hover:text-indigo-400">
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a href="/shop" className="hover:text-indigo-400">
+                    Products
+                  </a>
+                </li>
+                <li>
+                  <a href="/about-us" className="hover:text-indigo-400">
+                    About Us
+                  </a>
+                </li>
               </ul>
             </div>
             <div>
@@ -31,7 +42,7 @@ const Footer = () => {
         </div>
       </footer>
     </div>
-  )
-}
+  );
+};
 
 export default Footer;
