17b07b48136981a6718dac3a207053f8e55acb58
Mohammed <6bouass9@gmail.com>
2024-10-17 21:04:31 +0100

    Adjust color schemes across various pages for improved aesthetics
    - Modified `index.css` to establish a cohesive color palette that
    enhances the visual experience.
    - Updated the color styles in the following components:
    - `Cart.jsx`: Improved background and text colors for better
    readability.
    - `Home.jsx`: Adjusted header and footer colors for a more
    vibrant appearance.
    - `Favorites.jsx`, `FavoritesCount.jsx`, `HeartIcons.jsx`:
    Ensured color consistency to highlight favorite products
    effectively.
    - `Product.jsx`, `ProductCard.jsx`,
    	`ProductCarousel.jsx`: Fine-tuned color contrasts to
    	make product details stand out.
    	- `ProductDetails.jsx`, `ProductTabs.jsx`,
    	`SmallProduct.jsx`: Enhanced colors to improve
    	user engagement and navigation.
    	- `Shop.jsx`: Updated colors to create a more
    	inviting shopping experience.
    
    	These adjustments aim to create a more
    	visually appealing and user-friendly
    	interface.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
 
             <Link
               to="/shop"
-              className="bg-pink-600 font-bold rounded-full py-2 px-10 mr-[18rem] mt-[10rem]"
+              className="bg-blue-600 font-bold rounded-full py-2 px-10 mr-[18rem] mt-[10rem]"
             >
               Shop
             </Link>
--- frontend/src/pages/Shop.jsx
+++ frontend/src/pages/Shop.jsx
@@ -84,7 +84,7 @@ const Shop = () => {
     <>
       <div className="container mx-auto">
         <div className="flex md:flex-row">
-          <div className="bg-[#413e3e] p-3 mt-2 mb-2 rounded-3xl">
+          <div className="bg-[#2f32d1] p-3 mt-2 mb-2 rounded-3xl">
             <h2 className="h4 text-center py-2 bg-white rounded-full mb-2">
               Filter by Categories
             </h2>
@@ -97,7 +97,7 @@ const Shop = () => {
                       type="checkbox"
                       id="red-checkbox"
                       onChange={(e) => handleCheck(e.target.checked, c._id)}
-                      className="w-4 h-4 text-pink-600 bg-gray-100 border-gray-300 rounded focus:ring-pink-500 dark:focus:ring-pink-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                      className="w-4 h-4 text-blue-800 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                     />
 
                     <label
@@ -124,7 +124,7 @@ const Shop = () => {
                       id={brand}
                       name="brand"
                       onChange={() => handleBrandClick(brand)}
-                      className="w-4 h-4 text-pink-400 bg-gray-100 border-gray-300 focus:ring-pink-500 dark:focus:ring-pink-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                      className="w-4 h-4 text-blue-400 bg-blue-100 border-blue-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
                     />
 
                     <label
@@ -148,7 +148,7 @@ const Shop = () => {
                 placeholder="Enter Price"
                 value={priceFilter}
                 onChange={handlePriceChange}
-                className="w-full px-3 py-2 placeholder-gray-400 border rounded-lg focus:outline-none focus:ring focus:border-pink-300"
+                className="w-full px-3 py-2 placeholder-blue-400 border rounded-lg focus:outline-none focus:ring focus:border-blue-300"
               />
             </div>
 
