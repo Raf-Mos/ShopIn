2835d666d920e18d3745cc275c5a92e77401b611
Mohammed <6bouass9@gmail.com>
2024-10-17 17:21:51 +0100

    Add order management pages, shipping functionality, and admin dashboard
    - Introduced the `AdminDashboard.jsx` to provide an overview of order
    management for admin users.
    - Added `OrderList.jsx` to display all orders with relevant details
    for easy tracking and management.
    - Implemented order processing pages:
    - `PlaceOrder.jsx`: Facilitates the order placement process for
    users.
    - `Order.jsx`: Displays individual order details for users and
    admin.
    - Created a `Shipping.jsx` page to manage shipping options
    and details.
    - Added `UserOrder.jsx` for users to view their past
    orders and statuses.
    - Developed a progress steps component to enhance user
    experience during the order process:
    - `ProgressSteps.jsx`: Visualizes the order progress
    through various stages.
    - New components for product display:
    - `ProductCard.jsx`: A reusable component for
    showcasing individual products in various
    contexts (e.g., shop, orders).
    - Updated navigation to accommodate new pages
    and improve user flow.
    - Enhanced Redux store with `shopSlice.js`
    for managing shop-related states and
    `orderApiSlice.js` for API interactions
    related to orders.
    - Renamed `Home.jsx` to `Home.jsx` for
    improved file organization.
    
    This update enhances the order
    processing capabilities and provides
    admins with tools to manage orders
    efficiently, improving overall site
    functionality.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -21,9 +21,19 @@ import CategoryList from './pages/Admin/CategoryList.jsx';
 import ProductList from './pages/Admin/ProductList.jsx';
 import AllProducts from './pages/Admin/AllProducts.jsx';
 import ProductUpdate from './pages/Admin/ProductUpdate.jsx';
-import Home from './Home.jsx';
+import Home from './pages/Home.jsx';
 import Favorites from './pages/Products/Favorites.jsx';
 import ProductDetails from './pages/Products/ProductDetails.jsx';
+import Cart from './pages/Cart.jsx';
+import Shop from './pages/Shop.jsx';
+import Shipping from './pages/Orders/Shipping.jsx';
+import PlaceOrder from './pages/Orders/PlaceOrder.jsx';
+
+import {PayPalScriptProvider} from '@paypal/react-paypal-js'
+import Order from './pages/Orders/Order.jsx';
+import UserOrder from './pages/User/UserOrder.jsx';
+import OrderList from './pages/Admin/OrderList.jsx';
+import AdminDashboard from './pages/Admin/AdminDashboard.jsx';
 
 const router = createBrowserRouter(
 	createRoutesFromElements(
@@ -33,9 +43,15 @@ const router = createBrowserRouter(
 		<Route index={true} path='/' element={<Home />} />
 		<Route path="/favorite" element={<Favorites />} />
 		<Route path="/product/:id" element={<ProductDetails />} />
+		<Route path="/cart" element={<Cart />} />
+		<Route path="/shop" element={<Shop />} />
 
 		<Route path="" element={<PrivateRoute />}>
 		<Route path='/profile' element={<Profile />} />
+		<Route path='/shipping' element={<Shipping />} />
+		<Route path='/placeorder' element={<PlaceOrder />} />
+		<Route path='/order/:id' element={<Order />} />
+		<Route path='/user-orders' element={<UserOrder />} />
 		</Route>
 
 		{/* Admin Routes */}
@@ -45,13 +61,17 @@ const router = createBrowserRouter(
 			<Route path='productlist' element={<ProductList />} />
 			<Route path='allproductslist' element={<AllProducts />} />
 			<Route path='product/update/:_id' element={<ProductUpdate />} />
+			<Route path='orderlist' element={<OrderList />} />
+			<Route path='dashboard' element={<AdminDashboard />} />
 		</Route>
 		</Route>
 	)
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<Provider store={store}>
-	<RouterProvider router={router} />
-	</Provider>
+<Provider store={store}>
+	<PayPalScriptProvider>
+      <RouterProvider router={router} />
+    </PayPalScriptProvider>
+</Provider>
 )
