f8f75a66c56992896cd111dd8c7104b32668b011
Mohammed <6bouass9@gmail.com>
2024-10-18 00:23:12 +0100

    update admindashboard

--- frontend/src/pages/Admin/AdminDashboard.jsx
+++ frontend/src/pages/Admin/AdminDashboard.jsx
@@ -1,4 +1,3 @@
-import Chart from "react-apexcharts";
 import { useGetUsersQuery } from "../../redux/api/usersApiSlice";
 import {
   useGetTotalOrdersQuery,
@@ -6,7 +5,6 @@ import {
   useGetTotalSalesQuery,
 } from "../../redux/api/orderApiSlice";
 
-import { useState, useEffect } from "react";
 import AdminMenu from "./AdminMenu";
 import OrderList from "./OrderList";
 import Loader from "../../components/Loader";
@@ -15,78 +13,7 @@ const AdminDashboard = () => {
   const { data: sales, isLoading } = useGetTotalSalesQuery();
   const { data: customers, isLoading: loading } = useGetUsersQuery();
   const { data: orders, isLoading: loadingTwo } = useGetTotalOrdersQuery();
-  const { data: salesDetail } = useGetTotalSalesByDateQuery();
-
-  const [state, setState] = useState({
-    options: {
-      chart: {
-        type: "line",
-      },
-      tooltip: {
-        theme: "dark",
-      },
-      colors: ["#0F0F0F"],
-      dataLabels: {
-        enabled: true,
-      },
-      stroke: {
-        curve: "smooth",
-      },
-      title: {
-        text: "Sales Trend",
-        align: "left",
-      },
-      grid: {
-        borderColor: "#ccc",
-      },
-      markers: {
-        size: 1,
-      },
-      xaxis: {
-        categories: [],
-        title: {
-          text: "Date",
-        },
-      },
-      yaxis: {
-        title: {
-          text: "Sales",
-        },
-        min: 0,
-      },
-      legend: {
-        position: "top",
-        horizontalAlign: "right",
-        floating: true,
-        offsetY: -25,
-        offsetX: -5,
-      },
-    },
-    series: [{ name: "Sales", data: [] }],
-  });
-
-  useEffect(() => {
-    if (salesDetail) {
-      const formattedSalesDate = salesDetail.map((item) => ({
-        x: item._id,
-        y: item.totalSales,
-      }));
-
-      setState((prevState) => ({
-        ...prevState,
-        options: {
-          ...prevState.options,
-          xaxis: {
-            categories: formattedSalesDate.map((item) => item.x),
-          },
-        },
-
-        series: [
-          { name: "Sales", data: formattedSalesDate.map((item) => item.y) },
-        ],
-      }));
-    }
-  }, [salesDetail]);
+  const { data: salesByDate, isLoading: loadingThree } = useGetTotalSalesByDateQuery();
 
   return (
     <>
@@ -101,7 +28,7 @@ const AdminDashboard = () => {
 
             <p className="mt-5">Sales</p>
             <h1 className="text-xl font-bold">
-              $ {isLoading ? <Loader /> : sales.totalSales.toFixed(2)}
+              $ {isLoading ? <Loader /> : sales?.totalSales?.toFixed(2)}
             </h1>
           </div>
           <div className="rounded-lg bg-gray-200 p-5 w-[20rem] mt-5">
@@ -111,7 +38,7 @@ const AdminDashboard = () => {
 
             <p className="mt-5">Customers</p>
             <h1 className="text-xl font-bold">
-               {isLoading ? <Loader /> : customers?.length}
+               {loading ? <Loader /> : customers?.length}
             </h1>
           </div>
           <div className="rounded-lg bg-gray-200 p-5 w-[20rem] mt-5">
@@ -121,18 +48,9 @@ const AdminDashboard = () => {
 
             <p className="mt-5">All Orders</p>
             <h1 className="text-xl font-bold">
-               {isLoading ? <Loader /> : orders?.totalOrders}
+               {loadingTwo ? <Loader /> : orders?.totalOrders}
             </h1>
           </div>
-        </div>
-
-        <div className="ml-[10rem] mt-[4rem]">
-          <Chart
-            options={state.options}
-            series={state.series}
-            type="bar"
-            width="70%"
-          />
         </div>
 
         <div className="mt-[4rem]">
